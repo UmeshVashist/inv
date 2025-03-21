7c7decd298401c9d16909e36a4b43213bac5004d
UmeshVashist <us781819@gmail.com>
2025-03-21 21:50:41 +0530

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,7 @@
+// document.querySelectorAll('.drodown').forEach
+// (item=>{
+//     item.addEventListener('click',function(){
+
+//         this.querySelector('.dropdown-menu').classList.toggle('show');
+//     })
+// })
