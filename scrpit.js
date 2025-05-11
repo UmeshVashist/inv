7094adffcd99b7d30226bbdce11d3d4a65f847fb
UmeshVashist <us781819@gmail.com>
2025-05-11 13:59:36 +0530

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -1,7 +1,14 @@
-// document.querySelectorAll('.drodown').forEach
-// (item=>{
-//     item.addEventListener('click',function(){
+let button = document.getElementById("btn");
 
-//         this.querySelector('.dropdown-menu').classList.toggle('show');
-//     })
-// })
+button.addEventListener("click", () => {
+    const box = document.querySelector(".box");
+    box.innerHTML = `<b class="b1">If you want to add new link please contact on mentioned email!:- us781819@gmail.com!</b><br>
+        <b class="b2">Note:- When contacting by email, please mention the link or name of the website you want to add </b>
+    `;
+
+    // alert("When contacting by email, please mention the link or name of the website you want to add")
+    setTimeout(() => {
+        const boldTags = box.querySelectorAll("b");
+        boldTags.forEach(tag => tag.style.display = "none");
+    }, 10000);
+});
