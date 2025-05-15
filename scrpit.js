0708fb3a922ddc21b71ad32369296db817246ea8
UmeshVashist <us781819@gmail.com>
2025-05-15 12:14:11 +0530

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -2,8 +2,8 @@ let button = document.getElementById("btn");
 
 button.addEventListener("click", () => {
     const box = document.querySelector(".box");
-    box.innerHTML = `<b class="b1">If you want to add new link please contact on mentioned email!:- us781819@gmail.com!</b><br>
-        <b class="b2">Note:- When contacting by email, please mention the link or name of the website you want to add </b>
+    box.innerHTML = `<b class="b1">If you want to add new link please contact on mentioned email!:- <a href="us781819@gmail.com">us781819@gmail.com!</a></b><br>
+        <b class="b2">Note:- When contacting by email, please mention the link or name of the website you want to add!</b>
     `;
 
     // alert("When contacting by email, please mention the link or name of the website you want to add")
