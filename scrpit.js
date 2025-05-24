4940cfa5e9b5109fe7593846dc32bbd3659fdf2c
lbm <us781819@gmail.com>
2025-05-24 08:49:28 +0530

    Update scrpit.js

--- scrpit.js
+++ scrpit.js
@@ -2,7 +2,7 @@ let button = document.getElementById("btn");
 
 button.addEventListener("click", () => {
     const box = document.querySelector(".box");
-    box.innerHTML = `<b class="b1">If you want to add new link please contact on mentioned email!:- <a href="us781819@gmail.com">us781819@gmail.com!</a></b><br>
+    box.innerHTML = `<b class="b1">If you want to add new link please contact on mentioned email!:- <a href="">us781819@gmail.com!</a></b><br>
         <b class="b2">Note:- When contacting by email, please mention the link or name of the website you want to add!</b>
     `;
 
